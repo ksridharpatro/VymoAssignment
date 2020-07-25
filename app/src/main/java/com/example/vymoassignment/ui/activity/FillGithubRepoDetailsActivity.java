764321030da9af93e454baa004cabffae97c03ee
sridhar.patro <sridhar.patro@happay.in>
2020-07-25 16:45:26 +0530

    fill repo details screen designed

---
+++ app/src/main/java/com/example/vymoassignment/ui/activity/FillGithubRepoDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.vymoassignment.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vymoassignment.R;
+
+public class FillGithubRepoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_github_repo_details);
+    }
+}
